31c9f796096e6f395e06762cfff7be5c8936fe5c
Anais <learner-3ea63f@extensionschool.ch>
2020-08-26 19:28:32 +0200

    Models test for the user that asserts that a user is saved

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'a user can be saved' do 
+    user = User.new username: 'Name', 
+                    email: 'name@user.ch'
+    user.save!
+    assert_equal(User.first.username, user.username)
+    assert_equal(User.first.email, user.email)
+    assert_equal(User.all.length, 1)
+  end 
 end
