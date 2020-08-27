f25b876c424c5c574a18ed7f77b67c650b723a45
Anais <learner-3ea63f@extensionschool.ch>
2020-08-27 08:52:33 +0200

    Controller action to create and save a user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end 
+
+  def create 
+    user = User.new(user_params)
+    user.save!
+    redirect_to(user_path(user))
+  end 
+
+  def show 
+  end 
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:username, :email)
+  end 
+
+end
