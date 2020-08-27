9909d538f3e89d7ff46df304de82142f915d379f
Anais <learner-3ea63f@extensionschool.ch>
2020-08-27 10:32:47 +0200

    Crete a contextual menu visitor user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,10 @@
 class HomeController < ApplicationController
+
+  def index
+    @has_user_signed = session[:user_id].present?
+    if @has_user_signed
+      @user = session[:user_id]
+    end
+  end 
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,24 @@
 class UsersController < ApplicationController
 
   def new
+    @has_user_signed = session[:user_id].present?
+    if @has_user_signed
+      @user = session[:user_id]
+    end
     @user = User.new
   end 
 
   def create 
     user = User.find_or_create_by(user_params)
+    session[:user_id] = user.id
     redirect_to(user_path(user))
   end 
 
   def show 
+    @has_user_signed = session[:user_id].present?
+    if @has_user_signed
+      @user = User.find(session[:user_id])
+    end
   end 
 
   private 
