643511bb7775d708adb69e6faafe063747570cae
Anais <learner-3ea63f@extensionschool.ch>
2020-08-29 18:36:46 +0200

    Review validations and model test

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,10 @@
 class Review < ApplicationRecord
 
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :link, presence: true
+  validates :title, length: { maximum: 210 }
+
   belongs_to :user
 
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -21,4 +21,42 @@ class ReviewTest < ActiveSupport::TestCase
                         body: 'My review body'
     refute(review.save)
   end 
+
+  test 'a Review cannot be saved without a title' do
+    review = Review.new author: 'Podcast author', 
+                        image_url: 'https://is2-ssl.mzstatic.com/image/thumb/Podcasts113/v4/29/10/f0/2910f0bd-bea9-f4cc-d810-0c33a239af6c/mza_1582277213378144435.jpeg/1200x1200bb.jpg', 
+                        link: 'https://www.google.ch/', 
+                        body: 'My review body', 
+                        user: User.new
+    refute(review.save)
+  end 
+
+  test 'a Review cannot be saved without a link' do
+    review = Review.new title: 'A podcast title',
+                        author: 'Podcast author', 
+                        image_url: 'https://is2-ssl.mzstatic.com/image/thumb/Podcasts113/v4/29/10/f0/2910f0bd-bea9-f4cc-d810-0c33a239af6c/mza_1582277213378144435.jpeg/1200x1200bb.jpg', 
+                        body: 'My review body', 
+                        user: User.new
+    refute(review.save)
+  end 
+
+  test 'a Review cannot be saved without a body' do
+    review = Review.new title: 'A podcast title',
+                        author: 'Podcast author', 
+                        image_url: 'https://is2-ssl.mzstatic.com/image/thumb/Podcasts113/v4/29/10/f0/2910f0bd-bea9-f4cc-d810-0c33a239af6c/mza_1582277213378144435.jpeg/1200x1200bb.jpg', 
+                        link: 'https://www.google.ch/', 
+                        user: User.new
+    refute(review.save)
+  end 
+
+  test 'a Review cannot be saved without a title too long' do
+    review = Review.new title: 'Validations are used to ensure that only valid data is saved into your database. For example, it may be important to your application to ensure that every user provides a valid email address and mailing address. Model-level validations are the best way to ensure that only valid data is saved into your database. ',
+                        author: 'Podcast author', 
+                        image_url: 'https://is2-ssl.mzstatic.com/image/thumb/Podcasts113/v4/29/10/f0/2910f0bd-bea9-f4cc-d810-0c33a239af6c/mza_1582277213378144435.jpeg/1200x1200bb.jpg', 
+                        link: 'https://www.google.ch/', 
+                        body: 'My review body', 
+                        user: User.new
+    refute(review.save)
+  end 
+
 end
