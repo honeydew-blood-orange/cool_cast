840b86d8e88bb4712806fa41809b3501c79ba398
Anais <learner-3ea63f@extensionschool.ch>
2020-08-29 18:16:54 +0200

    Associate User and Review in model classes and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200829160748) do
+ActiveRecord::Schema.define(version: 20200829161525) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20200829160748) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
