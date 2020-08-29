e4343e972f7582f960666f14112c88c95c0795e8
Anais <learner-3ea63f@extensionschool.ch>
2020-08-29 19:54:05 +0200

    System tests for the review creation, user attribution and redirection

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,16 +19,23 @@ class ReviewsController < ApplicationController
     review = Review.new(review_params)
     review.user = User.find(session[:user_id])
     if (review.save)
-      redirect_to(review(review))
+      redirect_to(review_path(review))
     else
-      render new
+      render 'new'
     end 
+  end 
+
+  def show 
+    @has_user_signed = session[:user_id].present?
+    if @has_user_signed
+      @user = session[:user_id]
+    end
   end 
 
   private 
 
   def review_params 
-    params.require(review).permit(:title, :author, :image_url, :link, :body)
+    params.require(:review).permit(:title, :author, :image_url, :link, :body)
   end 
 
 end
--- test/system/reviews_test.rb
+++ test/system/reviews_test.rb
@@ -1,9 +1,52 @@
 require "application_system_test_case"
 
 class ReviewsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit reviews_url
-  #
-  #   assert_selector "h1", text: "Reviews"
-  # end
+  test 'a Review can be saved' do 
+    visit(new_user_path)
+    fill_in('Username', with: 'Username')
+    fill_in('Email', with: 'user@user.ch')
+    click_on('Log in')
+    visit(new_review_path)
+    fill_in('Podcast title', with: 'A podcast title')
+    fill_in('Podcast Author', with: 'An author')
+    fill_in('Podcast image', with: 'https://www.google.ch/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
+    fill_in('Podcast link', with: 'https://www.google.ch/')
+    fill_in('Your review', with: 'My review body')
+    click_on('Create Review')  
+    assert_equal(Review.all.length, 1)
+    assert_equal(Review.first.title, 'A podcast title')  
+  end 
+
+  test 'a Review is associated with a User' do 
+    user = User.new email:'user@user.ch', 
+                    username: 'Username'
+    user.save!
+    visit(new_user_path)
+    fill_in('Username', with: 'Username')
+    fill_in('Email', with: 'user@user.ch')
+    click_on('Log in')
+    visit(new_review_path)
+    fill_in('Podcast title', with: 'A podcast title')
+    fill_in('Podcast Author', with: 'An author')
+    fill_in('Podcast image', with: 'https://www.google.ch/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
+    fill_in('Podcast link', with: 'https://www.google.ch/')
+    fill_in('Your review', with: 'My review body')
+    click_on('Create Review')  
+    assert_equal(Review.first.user, user)  
+  end 
+
+  test 'a Review saved redirect to the review#show view' do 
+    visit(new_user_path)
+    fill_in('Username', with: 'Username')
+    fill_in('Email', with: 'user@user.ch')
+    click_on('Log in')
+    visit(new_review_path)
+    fill_in('Podcast title', with: 'A podcast title')
+    fill_in('Podcast Author', with: 'An author')
+    fill_in('Podcast image', with: 'https://www.google.ch/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
+    fill_in('Podcast link', with: 'https://www.google.ch/')
+    fill_in('Your review', with: 'My review body')
+    click_on('Create Review')  
+    assert current_path.include?(review_path(Review.first))
+  end 
 end
