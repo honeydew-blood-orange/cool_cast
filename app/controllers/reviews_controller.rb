3f59c0127ec9f13a411fc5bafa34e8bfce1bda6f
Anais <learner-3ea63f@extensionschool.ch>
2020-08-29 18:56:16 +0200

    Review controller and index view

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,10 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @has_user_signed = session[:user_id].present?
+    if @has_user_signed
+      @user = session[:user_id]
+    end
+  end
+
+end
