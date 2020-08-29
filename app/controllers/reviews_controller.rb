1e8f59b3ba51d6c766edff0c6323ff81d7df2f38
Anais <learner-3ea63f@extensionschool.ch>
2020-08-29 19:24:55 +0200

    New action in the review Controller and form partial and new view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,28 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def new 
+    @has_user_signed = session[:user_id].present?
+    if @has_user_signed
+      @user = session[:user_id]
+    end
+    @review = Review.new
+  end 
+
+  def create 
+    review = Review.new(review_params)
+    review.user = User.find(session[:user_id])
+    if (review.save)
+      redirect_to(review(review))
+    else
+      render new
+    end 
+  end 
+
+  private 
+
+  def review_params 
+    params.require(review).permit(:title, :author, :image_url, :link, :body)
+  end 
+
 end
