42738d8e928db09011208129c82560bc3d964e58
Anais <learner-3ea63f@extensionschool.ch>
2020-08-29 18:25:20 +0200

    Model tests for the review

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :users
+  resources :reviews
 
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'a Review can be saved' do
+    review = Review.new title: 'A podcast title', 
+                        author: 'Podcast author', 
+                        image_url: 'https://is2-ssl.mzstatic.com/image/thumb/Podcasts113/v4/29/10/f0/2910f0bd-bea9-f4cc-d810-0c33a239af6c/mza_1582277213378144435.jpeg/1200x1200bb.jpg', 
+                        link: 'https://www.google.ch/', 
+                        body: 'My review body', 
+                        user: User.new
+    review.save!
+    assert_equal(Review.all.length, 1)
+    assert_equal(Review.first.title, 'A podcast title')
+  end 
+
+  test 'a Review cannot be saved without a User' do
+    review = Review.new title: 'A podcast title', 
+                        author: 'Podcast author', 
+                        image_url: 'https://is2-ssl.mzstatic.com/image/thumb/Podcasts113/v4/29/10/f0/2910f0bd-bea9-f4cc-d810-0c33a239af6c/mza_1582277213378144435.jpeg/1200x1200bb.jpg', 
+                        link: 'https://www.google.ch/', 
+                        body: 'My review body'
+    refute(review.save)
+  end 
 end
