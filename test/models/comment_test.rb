5324c1feb4f03b4a0a06df97f01e11dad63daddb
Anais <learner-3ea63f@extensionschool.ch>
2020-08-30 20:10:52 +0200

    Model test for the Comment

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,6 +1,24 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  test '' do 
+  test 'a Comment can be saved' do 
+    comment = Comment.new body: 'Comment body', 
+                          user: User.new, 
+                          review: Review.new
+    comment.save!
+    assert_equal(Comment.all.length, 1)
+    assert_equal(Comment.first.body, 'Comment body')
+  end 
+
+  test 'a Comment cannot be saved without a User' do 
+    comment = Comment.new body: 'Comment body', 
+                          review: Review.new
+    refute(comment.save)
+  end 
+
+  test 'a Comment cannot be saved without a Review' do 
+    comment = Comment.new body: 'Comment body', 
+                          user: User.new    
+    refute(comment.save)
   end 
 end
