89b8c36c526db63cbad712b2e7c331461d1a8521
Anais <learner-3ea63f@extensionschool.ch>
2020-08-30 19:48:00 +0200

    Generate the Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200829161525) do
+ActiveRecord::Schema.define(version: 20200830174352) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
