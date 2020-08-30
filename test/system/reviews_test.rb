6599b27e3a4e22d44bc7451b017eac83b6299cad
Anais <learner-3ea63f@extensionschool.ch>
2020-08-30 17:52:14 +0200

    System test to check the display of the detail of the Review on the review#show view

--- test/system/reviews_test.rb
+++ test/system/reviews_test.rb
@@ -119,4 +119,18 @@ class ReviewsTest < ApplicationSystemTestCase
     assert page.has_content?('Updated author')
     assert page.has_content?('My review body updated')
   end 
+
+  test 'Review details are displayed on the review#show view' do 
+    review = Review.new title: 'A podcast title', 
+                        author: 'Podcast author', 
+                        image_url: 'https://is2-ssl.mzstatic.com/image/thumb/Podcasts113/v4/29/10/f0/2910f0bd-bea9-f4cc-d810-0c33a239af6c/mza_1582277213378144435.jpeg/1200x1200bb.jpg', 
+                        link: 'https://www.google.ch/', 
+                        body: 'My review body', 
+                        user: User.new
+    review.save!
+    visit(review_path(review))
+    assert page.has_content?('A podcast title')
+    assert page.has_content?('Podcast author')
+    assert page.has_content?('My review body')
+  end 
 end
